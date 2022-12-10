d030d57bce4f8c76b64f35591ef54694482843ea
devuongnull <59270866+Thanhan0901@users.noreply.github.com>
2022-12-11 01:36:00 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,43 +1,35 @@
 #!/bin/sh
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-    echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/Thanhan0901/install-proxy-v6/main/3proxy-0.9.3.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.3
+    version=0.9.3
+    apt-get update && apt-get -y upgrade
+    apt-get install gcc make git -y
+    wget --no-check-certificate -O 3proxy-${version}.tar.gz https://raw.githubusercontent.com/Thanhan0901/install-proxy-v6/main/3proxy-${version}.tar.gz
+    tar xzf 3proxy-${version}.tar.gz
+    cd 3proxy-${version}
     make -f Makefile.Linux
-    adduser --system --disabled-login --no-create-home --group proxy3
-    mkdir -p /var/log/3proxy
-    mkdir /etc/3proxy
-    cp bin/3proxy /usr/bin/
-    chown proxy3:proxy3 -R /etc/3proxy
-    chown proxy3:proxy3 /usr/bin/3proxy
-    chown proxy3:proxy3 /var/log/3proxy
-    cat>/etc/systemd/system/3proxy.service <<EOF
-    #service config
-[Unit]
-Description=3proxy Proxy Server
-[Service]
-Type=simple
-ExecStart=/usr/bin/3proxy /etc/3proxy/3proxy.cfg
-ExecStop=/bin/kill '/usr/bin/pgrep -u proxy3'
-RemainAfterExit=yes
-Restart=on-failure
-[Install]
-WantedBy=multi-user.target
-    EOF
-    systemctl daemon-reload
+    cd src
+    mkdir /etc/3proxy/
+    mv 3proxy /etc/3proxy/
+    cd /etc/3proxy/
+    wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+    chmod 600 /etc/3proxy/3proxy.cfg
+    mkdir /var/log/3proxy/
+    cd /etc/init.d/
+    wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+    chmod  +x /etc/init.d/3proxy
+    update-rc.d 3proxy defaults
     cd $WORKDIR
 }
 
@@ -71,11 +63,11 @@ upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
     URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-
+    
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
-
+    
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
@@ -85,7 +77,7 @@ gen_data() {
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
@@ -95,7 +87,6 @@ $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
-apt-get install gcc net-tools bsdtar zip -y
 
 install_3proxy
 
@@ -120,17 +111,9 @@ gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+gen_3proxy >/etc/3proxy/3proxy.cfg
 
-cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-systemctl enable 3proxy
-systemctl start 3proxy
-EOF
-
-bash /etc/rc.local
+/etc/init.d/3proxy start
 
 gen_proxy_file_for_user
 
